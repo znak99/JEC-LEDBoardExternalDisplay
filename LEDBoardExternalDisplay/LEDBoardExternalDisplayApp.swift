a3e538fad283390b0db047472258d60cf20f2ef3
znak99 <xhahekcldhkznak0113@gmail.com>
2023-06-06 00:26:51 +0900

    Initial Commit

---
+++ LEDBoardExternalDisplay/LEDBoardExternalDisplayApp.swift
@@ -0,0 +1,17 @@
+//
+//  LEDBoardExternalDisplayApp.swift
+//  LEDBoardExternalDisplay
+//
+//  Created by SeungWoo Hong on 2023/06/06.
+//
+
+import SwiftUI
+
+@main
+struct LEDBoardExternalDisplayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
