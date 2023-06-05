d92b702cb539439b5a995dd74c74d8a0d210a33b
znak99 <xhahekcldhkznak0113@gmail.com>
2023-06-06 00:54:25 +0900

    Add colors(ColorExtension): neonEmerald, neonSkyBlue, neonViolet

---
+++ LEDBoardExternalDisplay/Extensions/ColorExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ColorExtension.swift
+//  LEDBoardExternalDisplay
+//
+//  Created by SeungWoo Hong on 2023/06/06.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let neonEmerald = Color.init(red: 1/255, green: 1, blue: 195/255)
+    static let neonSkyBlue = Color.init(red: 1/255, green: 1, blue: 1)
+    static let neonViolet = Color.init(red: 157/255, green: 114/255, blue: 1)
+}
