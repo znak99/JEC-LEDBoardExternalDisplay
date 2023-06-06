cc4622cfae2acadd70885df7aac299864ece402c
znak99 <xhahekcldhkznak0113@gmail.com>
2023-06-07 01:39:15 +0900

    Create colorpicker viewmodel

---
+++ LEDBoardExternalDisplay/ViewModels/ColorPickerManager.swift
@@ -0,0 +1,12 @@
+//
+//  ColorPickerManager.swift
+//  LEDBoardExternalDisplay
+//
+//  Created by SeungWoo Hong on 2023/06/07.
+//
+
+import Foundation
+
+class ColorPickerManager: ObservableObject {
+    
+}
